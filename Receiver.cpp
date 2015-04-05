5bde711d477e6ecf8117d0c8160c436b1dc19689
myly10 <myly10@users.noreply.github.com>
2015-04-05 14:24:09 +0800

    Optimized for 3ms

--- Receiver.cpp
+++ Receiver.cpp
@@ -12,8 +12,6 @@ char ch=0;
 int signalValue;
 const int trueV=970, falseV=960;
 
-unsigned long previousMillis=0;        // will store last time LED was updated
-
 void setup(){
 	// set prescale to 16
 	sbi(ADCSRA, ADPS2);
@@ -27,46 +25,33 @@ void setup(){
 }
 
 void loop(){
-	//	unsigned long currentMillis=millis();
 	//clock high
 	while (analogRead(clockPin)<trueV){
-		//	Serial.println(analogRead(clockPin));
 	}
 	digitalWrite(13, HIGH);
 	delay(1);
 	signalValue=analogRead(signalPin);
-	//	Serial.println(signalValue);
 	while (signalValue<=trueV&&signalValue>=falseV){
 		signalValue=analogRead(signalPin);
-		//		Serial.println(signalValue);
 	}
 	if (signalValue>trueV) signalValue=1;
 	else if (signalValue<falseV) signalValue=0;
-	//	Serial.println(signalValue);
-	//	else Serial.println("ERROR");
 	ch|=signalValue<<bit++;
-	//	Serial.println("HIGH");
 
 	//clock low
 	while (analogRead(clockPin)>falseV){}
 	digitalWrite(13, LOW);
 	delay(1);
 	signalValue=analogRead(signalPin);
-	//	Serial.println(signalValue);
 	while (signalValue<=trueV&&signalValue>=falseV){
 		signalValue=analogRead(signalPin);
-		//		Serial.println(signalValue);
 	}
 	if (signalValue>trueV) signalValue=1;
 	else if (signalValue<falseV) signalValue=0;
-	//	Serial.println(signalValue);
-	//	else Serial.println("ERROR");
 	ch|=signalValue<<bit++;
-	//	Serial.println("LOW");
 
 	if (bit==8){
 		Serial.write(ch);
-		Serial.flush();
 		bit=0;
 		ch=0;
 	}
